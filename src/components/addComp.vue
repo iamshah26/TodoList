bf43113dc83a497d49058f7c7ae4a0f8e6ecabda
resume <syed.haider@voltro.com>
2022-11-18 18:08:15 +0500

    todolist

---
+++ src/components/addComp.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Hello  user, Welcome to add restruant</h1>
+</template>
+
+<script>
+export default {
+name:'addComp'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quidem omnis facere laudantium aliquam quia, unde
+      libero et alias soluta eius est minus eos labore natus? Nobis perspiciatis quos pariatur.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quidem omnis facere laudantium aliquam quia, unde
+      libero et alias soluta eius est minus eos labore natus? Nobis perspiciatis quos pariatur.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quidem omnis facere laudantium aliquam quia, unde
+      libero et alias soluta eius est minus eos labore natus? Nobis perspiciatis quos pariatur.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse quidem omnis facere laudantium aliquam quia, unde
+      libero et alias soluta eius est minus eos labore natus? Nobis perspiciatis quos pariatur.</p>
+  </div>
+</template>
